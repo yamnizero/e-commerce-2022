cb88558ec8fbc6f30a99090c64aca9ac467ad73a
mohammed jamal <yamni.zero@gmail.com>
2022-08-19 01:58:54 +0200

    forgettPassword

--- lib/link_api.dart
+++ lib/link_api.dart
@@ -4,10 +4,15 @@ class AppLinkApi{
 
   static const String test = "$server/test.php";
 
-        /// =======================Auth==================  ///
+  /// ======================= Auth ==================  ///
 
   static const String signUp = "$server/auth/signup.php";
   static const String login = "$server/auth/login.php";
   static const String verifyCodeSignUp = "$server/auth/verfiycode.php";
 
+/// ======================= ForgetPassword ==================  ///
+
+  static const String checkEmail = "$server/forgetpassword/checkemail.php";
+  static const String resetPassword = "$server/forgetpassword/resetpassword.php";
+  static const String verifyCodeForgetPassword = "$server/forgetpassword/new_verifycode.php";
 }
